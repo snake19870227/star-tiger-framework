5ee9ab69d382b9181bcd439c9392f15a3b064169
snake19870227 <buhuayang1987@foxmail.com>
2020-03-29 19:52:15 +0800

    feat(star-tiger-framework/star-tiger-spring-boot-autoconfigure): 调整采用主动生效方式

---
+++ star-tiger-spring-boot-autoconfigure/src/main/java/com/snake19870227/stiger/autoconfigure/annotation/EnableStarTiger.java
@@ -0,0 +1,21 @@
+package com.snake19870227.stiger.autoconfigure.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Import;
+import com.snake19870227.stiger.autoconfigure.StarTigerAutoConfiguration;
+
+/**
+ * @author Bu HuaYang (buhuayang1987@foxmail.com)
+ * @date 2020/03/29
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Import(StarTigerAutoConfiguration.class)
+public @interface EnableStarTiger {
+}
