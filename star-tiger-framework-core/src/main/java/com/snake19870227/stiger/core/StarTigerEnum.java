3e7c1ee0360506ee1967c32d1e923fd4cf0ac58a
snake19870227 <buhuayang1987@foxmail.com>
2020-08-26 23:05:02 +0800

    feat(star-tiger-admin): 公共枚举接口

---
+++ star-tiger-framework-core/src/main/java/com/snake19870227/stiger/core/StarTigerEnum.java
@@ -0,0 +1,12 @@
+package com.snake19870227.stiger.core;
+
+/**
+ * @author Bu HuaYang (buhuayang1987@foxmail.com)
+ * 2020/08/26
+ */
+public interface StarTigerEnum {
+
+    String getId();
+
+    String getName();
+}
